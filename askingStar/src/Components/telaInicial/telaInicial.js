bf52b102cf3729733b37d9ee9f942fa1cf028af2
End-Star01 <anavanessa162@gmail.com>
2023-05-02 20:20:32 -0300

    a

--- askingStar/src/Components/telaInicial/telaInicial.js
+++ askingStar/src/Components/telaInicial/telaInicial.js
@@ -1,6 +1,25 @@
 import * as React from 'react';
 import { Modal, Portal, Text, Button, Provider } from 'react-native-paper';
 
-const component = () => {
-    const [visible, setVisible] = React.useState(false)
+const TelaInicial =({navigation}) => {
+
+    const styles = StyleSheet.create({
+        stylebutton:{
+            backgroundColor: 'white',
+            borderRadius: 5
+    }}
+    )
+
+    return (
+
+        <View style={{flex: 1}}>
+            <ImageBackground style= {{flex: 1}} source={require('../../../img/background-ask.png')} resizeMode='cover' ></ImageBackground>
+            <View style= {{ gap: 10, justifyContent: 'center', flex: 1, alignItems: 'center' }}>
+   <View>
+    <Button style={{color: "white"}}>Jogar</Button> 
+    </View> 
+
+
+</View>
+ </View>
 }
