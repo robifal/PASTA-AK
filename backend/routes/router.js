8c264702a0855140f99ce82e8b48e4d9f9aae21d
Pedrix55 <pedrogabriel12gamer@outlook.com>
2023-05-19 13:09:06 -0300

    Update router.js

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -19,7 +19,7 @@ module.exports = router
 // Pergunta router
 
 const perguntaRouter = require("./perguntas");
-
+                                
 router.use('/', perguntaRouter);
 
 module.exports = router
