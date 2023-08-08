610e53415e1b79e03b8e76b6f4bccdf7d184899d
KlinsmannAluno <pedrogabriel12gamer@outlook.com>
2023-08-08 10:25:49 -0300

    BackEnd  Removed

--- backend/routes/cadastroProfessor.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-
-const CadastropfController = require("../controllers/cadastropfController");
-
-router.route('/cadastropf').post((req, res) => CadastropfController.create(req, res));
-
-
-module.exports = router
--- backend/routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const userController = require("../controllers/userController");
-
-router.route("/login").get((req, res) => userController.login(req, res));
-
-module.exports = router;
